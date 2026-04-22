e5852aeb7db245ea6bee322073cc1a36452069d5
Krypt0kn1t <intechin99@gmail.com>
2026-04-22 17:32:09 +0100

    finishing the basics and beginning the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
