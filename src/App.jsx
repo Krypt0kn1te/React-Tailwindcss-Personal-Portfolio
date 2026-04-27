52f9bfd743728a6043bc684e68cd6cda79716c99
Krypt0kn1t <intechin99@gmail.com>
2026-04-27 14:37:02 +0100

    finishing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "@/sections/Projects"
 import Experience from "@/sections/Experience"
 import Testimonials from "@/sections/Testimonials"
 import Contact from "@/sections/Contact"
+import { Footer } from './layout/Footer'
 
 
 
@@ -20,6 +21,8 @@ const App = () => {
         <Testimonials />
         <Contact />
       </main>
+
+      <Footer/>
     </div>
   )
 }
